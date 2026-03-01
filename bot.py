e4facb72468c8efee7caad86d98ab9363d9bc049
Elnur <elnuralahverdiyev975@gmail.com>
2026-03-01 22:05:10 +0400

    dm system added

--- bot.py
+++ bot.py
@@ -3,38 +3,33 @@ import discord
 from discord.ext import commands
 import asyncio
 
-# TOKEN Railway Variables-dən gəlir
 TOKEN = os.getenv("TOKEN")
 
-# INTENTS
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
-# BOT ONLINE
 @bot.event
 async def on_ready():
     print(f"{bot.user} online!")
-    bot.loop.create_task(keep_alive())
 
-
-# SERVERƏ GİRƏNİ KICK
 @bot.event
 async def on_member_join(member):
     await asyncio.sleep(5)
 
     if len(member.roles) == 1:
-        await member.kick(reason="Everyone only")
+        try:
+            await member.send(
+                """
+https://t.me/trowmarket
+https://discord.gg/rRN9X8RjfB
+"""
+            )
+        except:
+            pass
 
+        await member.kick(reason="No role")
 
-# RAILWAY SÖNDÜRMƏSİN DEYƏ
-async def keep_alive():
-    while True:
-        await asyncio.sleep(300)
-
-
-# BOT START
 bot.run(TOKEN)
