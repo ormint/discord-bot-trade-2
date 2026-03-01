33ec2230ba75fa405e7ab441cbe5ecc83d4dcc4e
Elnur <elnuralahverdiyev975@gmail.com>
2026-03-01 23:36:58 +0400

    update dm message

--- bot.py
+++ bot.py
@@ -6,27 +6,23 @@ TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.members = True
-intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} online!")
 
-
 @bot.event
 async def on_member_remove(member):
     try:
         await member.send(
-"""You have been removed from the server for v4n1ty protection. If you want to tr4de, enter this server .
-
-https://t.me/darkbring
-https://discord.gg/rRN9X8RjfB"""
+            "If you want to trade join this server https://discord.gg/rRN9X8RjfB"
         )
     except:
-        print("DM göndərilə bilmədi.")
-
+        pass
 
 bot.run(TOKEN)
