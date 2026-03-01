5359c4c1b3d048700854117b5bf249dd80ccd0ef
Elnur <elnuralahverdiyev975@gmail.com>
2026-03-01 21:43:07 +0400

    final bot

--- bot.py
+++ bot.py
@@ -1,29 +1,40 @@
 import os
 import discord
 from discord.ext import commands
+import asyncio
 
 # TOKEN Railway Variables-dən gəlir
 TOKEN = os.getenv("TOKEN")
 
-print("TOKEN:", TOKEN)  # yoxlama üçün
-
-# Intentlər
+# INTENTS
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Bot açıldı
+
+# BOT ONLINE
 @bot.event
 async def on_ready():
     print(f"{bot.user} online!")
+    bot.loop.create_task(keep_alive())
 
-# Serverə girənləri kick edir
+
+# SERVERƏ GİRƏNİ KICK
 @bot.event
 async def on_member_join(member):
+    await asyncio.sleep(5)
+
     if len(member.roles) == 1:
         await member.kick(reason="Everyone only")
 
-# BOTU BAŞLAT
+
+# RAILWAY SÖNDÜRMƏSİN DEYƏ
+async def keep_alive():
+    while True:
+        await asyncio.sleep(300)
+
+
+# BOT START
 bot.run(TOKEN)
