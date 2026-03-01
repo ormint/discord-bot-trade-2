04f06a92163474c972472bded1b999d2aca7cd10
Elnur <elnuralahverdiyev975@gmail.com>
2026-03-01 21:37:48 +0400

    final fix

--- bot.py
+++ bot.py
@@ -2,20 +2,28 @@ import os
 import discord
 from discord.ext import commands
 
+# TOKEN Railway Variables-dən gəlir
 TOKEN = os.getenv("TOKEN")
 
+print("TOKEN:", TOKEN)  # yoxlama üçün
+
+# Intentlər
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Bot açıldı
 @bot.event
 async def on_ready():
-    print("Bot online")
+    print(f"{bot.user} online!")
 
+# Serverə girənləri kick edir
 @bot.event
 async def on_member_join(member):
     if len(member.roles) == 1:
         await member.kick(reason="Everyone only")
 
+# BOTU BAŞLAT
 bot.run(TOKEN)
