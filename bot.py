bbc33ad523af5d3ad30ff650c88b8f30ecb5bf40
Elnur <elnuralahverdiyev975@gmail.com>
2026-03-02 00:18:21 +0400

    dm message updated

--- bot.py
+++ bot.py
@@ -1,33 +1,39 @@
 import os
 import discord
 from discord.ext import commands
+import asyncio
 
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.members = True
-intents.guilds = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
 @bot.event
 async def on_ready():
-    print(f"{bot.user} ONLINE")
-
+    print(f"{bot.user} online!")
 
 @bot.event
-async def on_member_remove(member):
-    print(f"{member} removed")
+async def on_member_join(member):
+    await asyncio.sleep(5)
 
-    try:
-        await member.send(
-            "If you want to trade join this server https://discord.gg/rRN9X8RjfB"
-        )
-        print("DM SENT ✅")
-    except Exception as e:
-        print("DM FAILED ❌")
-        print(e)
+    if len(member.roles) == 1:
+        try:
+            await member.send(
+                """
+You have been removed from the server for v4n1ty protection.
 
+If you want to tr4de, enter this server:
+https://discord.gg/rRN9X8RjfB
+
+https://t.me/darkbring
+"""
+            )
+        except:
+            pass
+
+        await member.kick(reason="v4n1ty protection")
 
 bot.run(TOKEN)
