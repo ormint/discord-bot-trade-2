d0884cf80736c58aea07167a567f0da58b9bac22
Elnur <elnuralahverdiyev975@gmail.com>
2026-03-01 21:13:43 +0400

    fix railway crash

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import os
 import discord
 from discord.ext import commands
-import asyncio
 
 TOKEN = os.getenv("TOKEN")
 
@@ -16,15 +15,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    await asyncio.sleep(6)
-
     if len(member.roles) == 1:
         await member.kick(reason="Everyone only")
-
-async def keep_alive():
-    while True:
-        await asyncio.sleep(3600)
-
-bot.loop.create_task(keep_alive())
 
 bot.run(TOKEN)
