5c78f066e14f4981b530a4d86362028857c477a8
Elnur <elnuralahverdiyev975@gmail.com>
2026-03-02 00:13:10 +0400

    fix dm

--- bot.py
+++ bot.py
@@ -13,27 +13,21 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} online!")
+    print(f"{bot.user} ONLINE")
 
 
 @bot.event
 async def on_member_remove(member):
+    print(f"{member} removed")
+
     try:
         await member.send(
             "If you want to trade join this server https://discord.gg/rRN9X8RjfB"
         )
-    except:
-        print("DM closed or failed")
-
-
-@bot.event
-async def on_member_ban(guild, user):
-    try:
-        await user.send(
-            "If you want to trade join this server https://discord.gg/rRN9X8RjfB"
-        )
-    except:
-        print("DM closed or failed")
+        print("DM SENT ✅")
+    except Exception as e:
+        print("DM FAILED ❌")
+        print(e)
 
 
 bot.run(TOKEN)
